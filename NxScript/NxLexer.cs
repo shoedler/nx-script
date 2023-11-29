1faa5cafd96871365af85a9aaa717ce5abd31292
Simon Schödler <simon.schoedler@nexplore.ch>
2023-11-29 13:35:24 +0100

    Adds test proj

--- NxScript/NxLexer.cs
+++ NxScript/NxLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from C:/Projects/nx-script///src//Nx.g4 by ANTLR 4.13.1
+// Generated from C:/Projects/nx-script/NxScript///Nx.g4 by ANTLR 4.13.1
 
 // Unreachable code detected
 #pragma warning disable 0162
